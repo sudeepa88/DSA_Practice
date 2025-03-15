328609d4e744d4c44c22663ea11fea66344a464b
sudeepa88 <palsudeepa@gmail.com>
2025-03-15 22:51:39 +0530

    Updated Binary Search

--- Binary Search/BinarySearchQ5.playground/Contents.swift
+++ Binary Search/BinarySearchQ5.playground/Contents.swift
@@ -9,25 +9,22 @@ func findKthPositive(_ arr: [Int], _ k: Int) -> Int  {
     
     var startingPoint = 0
     var endingpoint = arr.count - 1
-    var midPoint = endingpoint/2
-    
-    while startingPoint <= endingpoint {
-        var value = midPoint + 1
-       
+    var midPoint: Int = endingpoint/2
+            
+        while startingPoint <= endingpoint {
+            var value = midPoint + 1
+            
             var noOfMissingElement = arr[midPoint] - value
             if noOfMissingElement < k {
-                startingPoint = value
+                startingPoint = midPoint + 1
                 midPoint = startingPoint + ((endingpoint - startingPoint)/2)
-            } else if noOfMissingElement >= k {
+            }
+            else if noOfMissingElement >= k {
                 endingpoint = midPoint - 1
                 midPoint = startingPoint + ((endingpoint - startingPoint)/2)
             }
-    }
-    
-    var noOfMissingElement = arr[endingpoint] - (endingpoint + 1)
-    var desiredNumber = arr[endingpoint] + (  k - noOfMissingElement)
-    
-    return desiredNumber
+        } // while loop
+    return k + endingpoint + 1
 }
 
 
@@ -35,7 +32,8 @@ func findKthPositive(_ arr: [Int], _ k: Int) -> Int  {
 
 
 var arr = [2,3,4,7,11]
-var result = findKthPositive(arr, 3)
+//var arr = [3,7]
+var result = findKthPositive(arr, 4)
 print("The Result is ", result)
 
 
